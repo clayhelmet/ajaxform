3e36de1563b1c724b42294f60601d58a98668317
clayhelmet <113626476+clayhelmet@users.noreply.github.com>
2024-11-12 13:15:53 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// config.php
+
+$servername = "localhost";      // Usually 'localhost'
+$username = "root";    // Your MySQL username
+$password = "";    // Your MySQL password
+$dbname = "test20244";     // The database name created
+?>
